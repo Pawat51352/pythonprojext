07c17d4b9e18f4d0af4746642a1ff5e80e022c44
Pawat51352 <chanson_blogs_03@icloud.com>
2023-11-15 22:28:26 +0700

    Edited errors in date, month, and year.

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print('test')
+d = {'john':10, 'mary':20}
+d['tom'] = 30
+d['john'] = 40
+
+20 in d
---
+++ tester.py
@@ -0,0 +1,7 @@
+
+amount= 45000
+
+budget = int(input("Amount of money you want to save monthly"))
+while budget>amount:
+    budget = int(input("Amount of money you want to save monthly"))
+
